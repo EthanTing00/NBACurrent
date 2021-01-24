4600e3abe1326f6ddacb28bef30f195431b410b6
Ethan Ting <e.ting2000@hotmail.com>
2021-01-24 18:33:30 -0500

    Updated - deleted /css file in /static

--- NBADatabase/settings.py
+++ NBADatabase/settings.py
@@ -124,6 +124,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'app/static'),
-#)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'app/static'),
+)
