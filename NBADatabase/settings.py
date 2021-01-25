637b4b627494cbf168357178fe48a537d6bcbfb3
Ethan Ting <e.ting2000@hotmail.com>
2021-01-24 21:17:33 -0500

    UPDATED SETTINGS

--- NBADatabase/settings.py
+++ NBADatabase/settings.py
@@ -122,8 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'app')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/Documents/NBADatabase/NBADatabase/app/static/'
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'app/static'),
-#)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'app/static/'),
+)
