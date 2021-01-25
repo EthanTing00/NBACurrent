d0c1a57bebf6cb24db6e455fc41837bc53b36d9b
Ethan Ting <e.ting2000@hotmail.com>
2021-01-24 20:53:23 -0500

    UPDATED SETTINGS

--- NBADatabase/settings.py
+++ NBADatabase/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'a1ny@d85=%q@)rwvde9xhu4d7u#9*f9o!b_yfa+o3llbz_b=2)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nbacurrent.herokuapp.com']
 
 STATICFILES_DIRS = [
     "/Documents/NBADatabase/NBADatabase/app/static"
